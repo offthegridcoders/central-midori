f5828f5d02b6a3a1b7dc8c2384ab4880f52d479f
theWickedWebDev <syoung@paintnite.com>
2017-10-05 21:25:18 -0400

    lots of work on homepage

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,3 +2,14 @@
 jQuery(function($) {$('.slider').sss({
   slideShow : true
 });});
+
+function tradeShowNotify(){
+  notif({
+    type: "info",
+    msg: "<p>We are attending a trade show Oct 15th, 2017<br/>Click <a href='#'>here</a> for more information.</a></p>",
+    width: 500,
+    height: 100,
+    position: "bottom",
+    autohide: false
+  });
+}
